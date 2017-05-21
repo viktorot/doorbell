97f8020cb76fbada85ffbe72468e9f5700f90828
Unknown <viktorcc@gmail.com>
2017-05-21 15:43:30 +0200

    - electron server

---
+++ dorbell-electron-server/README.md
@@ -0,0 +1,1 @@
+# dorbell-electron-server
---
+++ dorbell-electron-server/app/app.js
@@ -0,0 +1,77 @@
+module.exports = () => {
+  // Load The FS Module & The Config File
+  fs = require('fs');
+
+  // Load The Path Module
+  path = require('path');
+
+  config = JSON.parse(fs.readFileSync('config.json'));
+
+  // Load Express Module
+  express = require('express');
+  app = express();
+
+  // Load Body Parser Module
+  bodyParser = require('body-parser');
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: false}));
+
+  // Load Express Handlebars Module & Setup Express View Engine
+  expressHandlebars = require('express-handlebars');
+  app.set('views', __dirname+'/views/'); // Set The Views Directory
+  app.engine('html', expressHandlebars({ // Setup View Engine Middleware
+    layoutsDir:__dirname + '/views/layouts',
+  	defaultLayout: 'main',
+  	extname: '.html',
+  	helpers: {
+  		section: function(name, options) {
+
+  			if(!this._sections) {
+  				this._sections = {};
+  			}
+
+  			this._sections[name] = options.fn(this);
+  			return null;
+  		}
+  	}
+  }));
+  
+  app.set('view engine', 'html');
+
+  // Setup Globally Included Directories
+  app.use(express.static(path.join(__dirname, '/../bower_components/')));
+  app.use(express.static(path.join(__dirname, '/../node_modules/')));
+  app.use(express.static(path.join(__dirname, '/../controllers/')));
+  app.use(express.static(path.join(__dirname, '/../public/')));
+
+  // Load Available Modules For Dependancy Injection Into Models & Routes
+  modules = {
+  	app: app,
+  	bodyParser: bodyParser,
+  	config: config,
+  	express: express,
+  	expressHandlebars: expressHandlebars,
+  	fs: fs,
+  	path: path
+  };
+
+  // Setup Globally Included Routes
+  fs.readdirSync(path.join(__dirname, 'routes')).forEach(function(filename) {
+  	if(~filename.indexOf('.js'))
+  		require(path.join(__dirname, 'routes/'+filename))(modules);
+  });
+
+  const notifier = require('node-notifier');
+  app.get('/ring', function(req, res) {
+    notifier.notify({
+        title: 'Title',
+        message: 'Hello from node, Mr. User!'
+    })
+
+    console.log('ringing...')
+    res.send("ringing...")
+  });
+
+  // Start The HTTP Server
+  app.listen(config.server.port, config.server.host);
+}
